a26694ff6bbabacaf69518b3aab55cf3e8dca7fa
yukisaha <0412judy@gmail.com>
2024-05-20 20:22:47 +0900

    Initial commit

---
+++ src/main/java/com/elice/tripnote/TripnoteApplication.java
@@ -0,0 +1,13 @@
+package com.elice.tripnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripnoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elice/tripnote/TripnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elice.tripnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
