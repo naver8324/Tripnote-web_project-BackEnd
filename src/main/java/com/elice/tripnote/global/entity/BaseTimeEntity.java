3418ef9bf7fd534825eefa87cd4e8a2c7eaf3104
yukisaha <0412judy@gmail.com>
2024-05-21 16:49:22 +0900

    feat: 초기세팅

---
+++ src/main/java/com/elice/tripnote/global/entity/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.elice.tripnote.global.entity;
+
+
+//import jakarta.persistence.Column;
+//import jakarta.persistence.EntityListeners;
+//import lombok.Getter;
+//import org.springframework.data.annotation.CreatedDate;
+//import org.springframework.data.annotation.LastModifiedDate;
+//import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+//
+//@EntityListeners(value = {AuditingEntityListener.class}) //Auditing 적용
+//@Getter
+public class BaseTimeEntity {
+//
+//    @CreatedDate
+//    @Column(updatable = false)
+//    private LocalDateTime creatAt; //등록일
+//
+//    @LastModifiedDate
+//    private LocalDateTime modifiedAt; //수정일
+//
+}
