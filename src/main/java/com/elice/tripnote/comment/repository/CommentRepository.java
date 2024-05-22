b0749cdb59f08b4454c525a4bf539be64890b199
BugeonJo <bugeonjo91@gmail.com>
2024-05-22 21:45:18 +0900

    FEAT: comment 서비스 단 일부 구현

--- src/main/java/com/elice/tripnote/comment/repository/CommentRepository.java
+++ src/main/java/com/elice/tripnote/comment/repository/CommentRepository.java
@@ -2,9 +2,21 @@ package com.elice.tripnote.comment.repository;
 
 
 import com.elice.tripnote.comment.entity.Comment;
+import com.elice.tripnote.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    List<Comment> findByPost_Id(Long postId);
+    Page<Comment> findByPost_Id(Long postId, Pageable pageable);
+//    List<User> findByUser_Id(Long userId);
+//    Page<User> findByUser_Id(Long userId, Pageable pageable);
+    Page<Comment> findAll(Pageable pageable);
 }
--- src/main/java/com/elice/tripnote/post/repository/PostRepository.java
+++ src/main/java/com/elice/tripnote/post/repository/PostRepository.java
@@ -5,6 +5,9 @@ import com.elice.tripnote.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
 }
