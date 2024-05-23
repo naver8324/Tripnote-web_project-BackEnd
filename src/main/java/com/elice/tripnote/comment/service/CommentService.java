12c4306362f2dc5c3ca43531bb306244c0677e33
BugeonJo <bugeonjo91@gmail.com>
2024-05-23 15:32:45 +0900

    fix: comment repository와 service단에서 user-> member 이후 주석처리된 유저 관련 메서드 주석 해제 및 변경

--- src/main/java/com/elice/tripnote/comment/service/CommentService.java
+++ src/main/java/com/elice/tripnote/comment/service/CommentService.java
@@ -4,13 +4,14 @@ import com.elice.tripnote.comment.entity.CommentRequestDTO;
 import com.elice.tripnote.comment.entity.Comment;
 import com.elice.tripnote.comment.entity.CommentResponseDTO;
 import com.elice.tripnote.comment.repository.CommentRepository;
+import com.elice.tripnote.member.entity.Member;
+import com.elice.tripnote.member.repository.MemberRepository;
+import com.elice.tripnote.post.exception.NoSuchAuthorizationException;
 import com.elice.tripnote.post.exception.NoSuchCommentException;
 import com.elice.tripnote.post.exception.NoSuchPostException;
 import com.elice.tripnote.post.entity.Post;
 import com.elice.tripnote.post.exception.NoSuchUserException;
 import com.elice.tripnote.post.repository.PostRepository;
-import com.elice.tripnote.user.entity.User;
-import com.elice.tripnote.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -30,22 +31,9 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
     private final PostRepository postRepository;
-    private final UserRepository userRepository;
+    private final MemberRepository memberRepository;
 
 
-
-    // 게시글에서 게시글에 해당하는 댓글을 불러올 때 사용하는 메서드. 삭제되지 않은 댓글만 불러옵니다.
-
-    @Transactional(readOnly = true)
-    public List<CommentResponseDTO> getCommentsByPostId(Long postId){
-
-        Post post = postOrElseThrowsException(postId);
-
-
-        return post.getComments().stream().filter(comment-> comment.isDeleted() == false).map(Comment::toDTO).collect(Collectors.toList());
-
-
-    }
 
     // 게시글에서 게시글에 해당하는 댓글을 페이지 형태로 불러올 때 사용하는 메서드. 삭제되지 않은 댓글만 불러옵니다.
 
@@ -72,38 +60,36 @@ public class CommentService {
 
     }
 
-    //user쪽 toDTO와 custom Exception이 완성 되면 주석을 해제할 예정입니다.
-//    public List<CommentResponseDTO> getCommentsByUserId(Long userId, int page, int size){
-//
-//        User user = userRepository.findById(userId).orElse(null);
-//        if(user == null){
-//            NoSuchUserException ex = new NoSuchUserException();
-//
-//            log.error("에러 발생: {}", ex.getMessage(), ex);
-//            throw ex;
-//        }
-//
-//        return commentRepository.findByUser_Id(userId, PageRequest.of(page, size, Sort.by("id").descending())).map(User::toDTO);
-//
-//
-//
-//    }
+    // 관리자가 한 유저의 전체 댓글을 불러올 때 사용하는 메서드. 삭제된 댓글도 불러옵니다.
+    public Page<CommentResponseDTO> getCommentsByMemberId(Long memberId, int page, int size){
+
+        Member member = memberOrElseThrowsException(memberId);
+
+        return commentRepository.findByMember_Id(memberId, PageRequest.of(page, size, Sort.by("id").descending())).map(Comment::toDTO);
+
+
+
+    }
 
 
     // 댓글을 저장하는 메서드입니다.
-    public CommentResponseDTO saveComment(CommentRequestDTO commentDTO, Long postId){
+    public CommentResponseDTO saveComment(CommentRequestDTO commentDTO, Long postId, Long memberId){
 
         Post post = postOrElseThrowsException(postId);
+        Member member = memberOrElseThrowsException(memberId);
+
 
         Comment comment = Comment.builder()
                 .content(commentDTO.getContent())
                 .post(post)
+                .member(member)
                 .build();
 
         // comment를 먼저 저장하고 add를 저장해야 EntityNotFoundException이 발생하지 않는다.
 
         commentRepository.save(comment);
         post.getComments().add(comment);
+        member.getComments().add(comment);
 
         return comment.toDTO();
 
@@ -143,14 +129,12 @@ public class CommentService {
 
 
     // 댓글을 삭제하는 메서드입니다. 댓글을 쓴 유저가 사용합니다.
-    public void deleteComment(Long commentId, Long userId){
+    public void deleteComment(Long commentId, Long memberId){
 
-        User user = userOrElseThrowsException(userId);
+        Comment comment = commentOrElseThrowsException(commentId);
 
-        Comment comment = commentRepository.findByIdAndUser_Id(commentId, userId);
-
-        if(comment == null){
-            NoSuchCommentException ex = new NoSuchCommentException();
+        if(comment.getMember().getId() == memberId){
+            NoSuchAuthorizationException ex = new NoSuchAuthorizationException();
             log.error("에러 발생: {}", ex.getMessage(), ex);
             throw ex;
         }
@@ -181,7 +165,6 @@ public class CommentService {
 
 
     // post id로 post를 불러 올 때 존재하면 post 객체를 반환하고 없으면 에러를 반환하는 메서드입니다.
-    @Transactional(readOnly = true)
     private Post postOrElseThrowsException(Long postId) {
 
         return postRepository.findById(postId)
@@ -193,7 +176,6 @@ public class CommentService {
     }
 
     // comment id로 comment를 불러 올 때 존재하면 comment 객체를 반환하고 없으면 에러를 반환하는 메서드입니다.
-    @Transactional(readOnly = true)
     private Comment commentOrElseThrowsException(Long commentId) {
 
         return commentRepository.findById(commentId)
@@ -204,11 +186,10 @@ public class CommentService {
                 });
     }
 
-    // user id로 user를 불러 올 때 존재하면 user 객체를 반환하고 없으면 에러를 반환하는 메서드입니다.
-    @Transactional(readOnly = true)
-    private User userOrElseThrowsException(Long userId) {
+    // member id로 member를 불러 올 때 존재하면 member 객체를 반환하고 없으면 에러를 반환하는 메서드입니다.
+    private Member memberOrElseThrowsException(Long memberId) {
 
-        return userRepository.findById(userId)
+        return memberRepository.findById(memberId)
                 .orElseThrow(() -> {
                     NoSuchUserException ex = new NoSuchUserException();
                     log.error("에러 발생: {}", ex.getMessage(), ex);
