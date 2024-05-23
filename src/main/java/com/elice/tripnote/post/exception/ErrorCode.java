d0904d2c4efe1f0a14499644f1b4043376bcb1d0
BugeonJo <bugeonjo91@gmail.com>
2024-05-23 14:27:20 +0900

    FEAT: 에러 코드 및 예외 추가, 그리고 임시 예외 추가

--- src/main/java/com/elice/tripnote/post/exception/ErrorCode.java
+++ src/main/java/com/elice/tripnote/post/exception/ErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    NO_POST(HttpStatus.NOT_FOUND, "해당하는 게시글은 존재하지 않습니다.");
+    NO_POST(HttpStatus.NOT_FOUND, "해당하는 게시글은 존재하지 않습니다."),
+    NO_COMMENT(HttpStatus.NOT_FOUND, "해당하는 댓글은 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
