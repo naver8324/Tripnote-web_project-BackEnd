7736220cce0cbd53ad0afe6becc2f8444e8de8d1
BugeonJo <bugeonjo91@gmail.com>
2024-05-23 14:25:30 +0900

    FEAT: Comment 서비스 구현, repository 일부 메서드 이름 변경 close #8

--- src/main/java/com/elice/tripnote/comment/repository/CommentRepository.java
+++ src/main/java/com/elice/tripnote/comment/repository/CommentRepository.java
@@ -3,6 +3,7 @@ package com.elice.tripnote.comment.repository;
 
 import com.elice.tripnote.comment.entity.Comment;
 import com.elice.tripnote.post.entity.Post;
+import com.elice.tripnote.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,7 +16,8 @@ import java.util.Optional;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findByPost_Id(Long postId);
-    Page<Comment> findByPost_Id(Long postId, Pageable pageable);
+    Page<Comment> findByPost_IdAndIsDeletedIsFalse(Long postId, Pageable pageable);
+    Comment findByIdAndUser_Id(Long id, Long userId);
 //    List<User> findByUser_Id(Long userId);
 //    Page<User> findByUser_Id(Long userId, Pageable pageable);
     Page<Comment> findAll(Pageable pageable);
