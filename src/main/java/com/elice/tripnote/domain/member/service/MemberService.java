70572063c5cde0e6c4e8220b1ed426b4804b48d9
[Cloud2] 정주용 <dusk3236@gmail.com>
2024-05-27 05:28:52 +0900

    feat: 로그인 시 jwt 발급 구현 #14

--- src/main/java/com/elice/tripnote/domain/member/service/MemberService.java
+++ src/main/java/com/elice/tripnote/domain/member/service/MemberService.java
@@ -1,22 +1,32 @@
 package com.elice.tripnote.domain.member.service;
 
 import com.elice.tripnote.domain.member.entity.Member;
+import com.elice.tripnote.domain.member.entity.MemberDetailsDTO;
 import com.elice.tripnote.domain.member.entity.MemberRequestDTO;
 import com.elice.tripnote.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public void joinProcess(MemberRequestDTO memberRequestDTO) {
+
+    // 회원가입 서비스
+    @Transactional
+    public void signup(MemberRequestDTO memberRequestDTO) {
 
         String email = memberRequestDTO.getEmail();
         String password = memberRequestDTO.getPassword();
@@ -40,4 +50,26 @@ public class MemberService {
         memberRepository.save(member);
     }
 
+
+    // 이메일로 회원 조회 서비스
+    @Transactional(readOnly = true)
+    public Member getMemberByEmail(String email) {
+        return memberRepository.findByEmail(email).orElse(null);
+    }
+
+
+    // 이메일 중복 체크 서비스
+    @Transactional(readOnly = true)
+    public boolean checkEmailDuplicate(String email) {
+        return memberRepository.existsByEmail(email);
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Member> memberData = memberRepository.findByEmail(email);
+
+        return memberData.map(MemberDetailsDTO::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+    }
 }
