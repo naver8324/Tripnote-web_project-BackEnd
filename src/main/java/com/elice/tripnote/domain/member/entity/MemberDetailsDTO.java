aee165beceac0644bd64752c98be898230f881dc
[Cloud2] 정주용 <dusk3236@gmail.com>
2024-05-27 05:28:23 +0900

    feat: entity 설계 및 enum 타입 설정 #14

---
+++ src/main/java/com/elice/tripnote/domain/member/entity/MemberDetailsDTO.java
@@ -0,0 +1,89 @@
+package com.elice.tripnote.domain.member.entity;
+
+import com.elice.tripnote.domain.admin.entity.Admin;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+public class MemberDetailsDTO implements UserDetails {
+
+    private final Member member;
+    private final Admin admin;
+
+    public MemberDetailsDTO(Member member) {
+        this.member = member;
+        this.admin = null;
+    }
+
+    public MemberDetailsDTO(Admin admin) {
+        this.member = null;
+        this.admin = admin;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                if (admin != null) {
+                    return "ROLE_ADMIN";
+                } else if (member != null) {
+                    return "ROLE_USER";
+                }
+                return "ROLE_UNKNOWN";
+            }
+        });
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        if (admin != null) {
+//            return admin.getPassword();  //admin getter 추가시 주석해제
+            return "12345678"; // 임시 비밀번호
+        } else if (member != null) {
+            return member.getPassword();
+        }
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        if (admin != null) {
+//            return admin.getLogin_id();  //admin getter 추가시 주석해제
+            return "admin"; // 임시 아이디
+        } else if (member != null) {
+            return member.getEmail();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        if (member != null) {
+            return member.getStatus() == Status.ACTIVE;
+        }
+        return true;
+    }
+}
