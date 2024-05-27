0724f1e54ca76b2650d87339c22bdd77967dee87
BugeonJo <bugeonjo91@gmail.com>
2024-05-27 12:31:16 +0900

    feat: 북마크 엔티티 변경, 메서드 추가

---
+++ src/main/java/com/elice/tripnote/domain/comment/controller/SwaggerCommentController.java
@@ -0,0 +1,7 @@
+package com.elice.tripnote.domain.comment.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+public interface SwaggerCommentController {
+}
--- src/main/java/com/elice/tripnote/domain/link/bookmark/repository/BookmarkRepository.java
+++ src/main/java/com/elice/tripnote/domain/link/bookmark/repository/BookmarkRepository.java
@@ -2,9 +2,12 @@ package com.elice.tripnote.domain.link.bookmark.repository;
 
 
 import com.elice.tripnote.domain.link.bookmark.entity.Bookmark;
+import com.elice.tripnote.domain.link.reportPost.entity.ReportPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+    Bookmark findByPostIdAndMemberId(Long postId, Long memberId);
+
 }
