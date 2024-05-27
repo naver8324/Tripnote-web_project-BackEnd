82e9176d4fe70b42810bfcbb66d61333e66a8bee
Kim Soo Hyun <spancer1@naver.com>
2024-05-27 16:12:14 +0900

    Implement SpotDTO

--- src/main/java/com/elice/tripnote/domain/spot/dto/SpotRequestDTO.java
+++ src/main/java/com/elice/tripnote/domain/spot/dto/SpotRequestDTO.java
@@ -1,2 +1,12 @@
-package com.elice.tripnote.domain.spot.dto;public class SpotRequestDTO {
+package com.elice.tripnote.domain.spot.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class SpotRequestDTO {
+    @NotBlank
+    private String region;
 }
