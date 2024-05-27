cded0c27d11bfba299f805be8bac726474c6fcdb
BugeonJo <bugeonjo91@gmail.com>
2024-05-27 17:58:28 +0900

    feat: 댓글 컨트롤러 구현. memberId 관련해서는 추후 수정 예정

--- src/main/java/com/elice/tripnote/domain/comment/controller/CommentController.java
+++ src/main/java/com/elice/tripnote/domain/comment/controller/CommentController.java
@@ -1,8 +1,11 @@
 package com.elice.tripnote.domain.comment.controller;
 
 
+import com.elice.tripnote.domain.comment.entity.CommentRequestDTO;
 import com.elice.tripnote.domain.comment.entity.CommentResponseDTO;
 import com.elice.tripnote.domain.comment.service.CommentService;
+import com.elice.tripnote.domain.post.exception.NoSuchAuthorizationException;
+import com.elice.tripnote.domain.post.exception.NoSuchCommentException;
 import com.elice.tripnote.domain.post.exception.NoSuchPostException;
 import com.elice.tripnote.domain.post.exception.NoSuchUserException;
 import com.elice.tripnote.global.entity.ErrorResponse;
@@ -38,6 +41,39 @@ public class CommentController implements SwaggerCommentController {
         return ResponseEntity.ok().body(commentService.getCommentsByMemberId(memberId, page, size));
     }
 
+    @Override
+    @PostMapping("/api/user/comments")
+    public ResponseEntity<CommentResponseDTO> saveComment(@RequestBody CommentRequestDTO commentDTO, @RequestParam(name="postId") Long postId, @RequestParam(name="memberId") Long memberId) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(commentService.saveComment(commentDTO, postId, memberId));
+    }
+
+    @Override
+    @PatchMapping("/api/user/comments/{commentId}")
+    public ResponseEntity<CommentResponseDTO> updateComment(@RequestBody CommentRequestDTO commentDTO, @PathVariable(name="commentId") Long commentId, @RequestParam(name="memberId") Long memberId) {
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.updateComment(commentDTO, commentId, memberId));
+    }
+
+    @Override
+    @GetMapping("/api/user/comments/{commentId}/report")
+    public ResponseEntity reportComment(@PathVariable(name="commentId") Long commentId, @RequestParam(name="memberId") Long memberId) {
+        commentService.reportComment(commentId, memberId);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @Override
+    @DeleteMapping("/api/user/comments/{commentId}")
+    public ResponseEntity deleteComment(@PathVariable(name="commentId") Long commentId, @RequestParam(name="memberId") Long memberId) {
+        commentService.deleteComment(commentId, memberId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @Override
+    @DeleteMapping("/api/admin/comments/{commentId}")
+    public ResponseEntity deleteComment(@PathVariable(name="commentId") Long commentId) {
+        commentService.deleteComment(commentId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
 
     @ExceptionHandler(NoSuchPostException.class)
     public ResponseEntity<ErrorResponse> handleNoSuchPostException(NoSuchPostException ex){
@@ -58,4 +94,27 @@ public class CommentController implements SwaggerCommentController {
                 .build();
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
+
+    @ExceptionHandler(NoSuchCommentException.class)
+    public ResponseEntity<ErrorResponse> handleNoSuchCommentException(NoSuchCommentException ex){
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .errorCode(404)
+                .message(ex.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(NoSuchAuthorizationException.class)
+    public ResponseEntity<ErrorResponse> handleNoSuchAuthorizationException(NoSuchAuthorizationException ex){
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .errorCode(401)
+                .message(ex.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+
+
 }
