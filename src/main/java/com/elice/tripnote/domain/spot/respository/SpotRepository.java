806ac124bf11d3d72de651f51b7fecda91b737e8
Kim Soo Hyun <spancer1@naver.com>
2024-05-27 16:14:59 +0900

    Implement SpotRepo

--- src/main/java/com/elice/tripnote/domain/spot/respository/SpotRepository.java
+++ src/main/java/com/elice/tripnote/domain/spot/respository/SpotRepository.java
@@ -1,2 +1,19 @@
-package com.elice.tripnote.domain.spot.respository;public class SpotRepository {
+package com.elice.tripnote.domain.spot.respository;
+
+import com.elice.tripnote.domain.spot.entity.Spot;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SpotRepository extends JpaRepository<Spot, Long> {
+
+    List<Spot> findAll();
+
+    Optional<Spot> findById(Long id);
+
+    List<Spot> findByRegion(String region);
 }
+
