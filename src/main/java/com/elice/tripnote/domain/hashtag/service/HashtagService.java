f3b4c4e17505d9fb1e9c22b1bdea79d7f56b97f9
yukisaha <0412judy@gmail.com>
2024-05-27 14:00:56 +0900

    feat: 해시태그 controller 추가

--- src/main/java/com/elice/tripnote/domain/hashtag/service/HashtagService.java
+++ src/main/java/com/elice/tripnote/domain/hashtag/service/HashtagService.java
@@ -5,14 +5,13 @@ import com.elice.tripnote.domain.hashtag.entity.HashtagRequestDTO;
 import com.elice.tripnote.domain.hashtag.entity.HashtagResponseDTO;
 import com.elice.tripnote.domain.hashtag.repository.HashtagRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class HashtagService {
 
     private final HashtagRepository hashtagRepository;
@@ -20,6 +19,8 @@ public class HashtagService {
     //해시태그 저장
     @Transactional
     public HashtagResponseDTO saveHashtag(HashtagRequestDTO hashtagRequestDTO){
+
+        log.info("hashtagRequestDTO.isCity = {}", hashtagRequestDTO.isCity());
 
         Hashtag hashtag = Hashtag.builder()
                 .name(hashtagRequestDTO.getName())
@@ -31,28 +32,23 @@ public class HashtagService {
         return hashtag.toResponseDTO();
     }
 
-    //해시태그 조회
+    //해시태그 조회 - repository에서 dto로 값을 반환해 바로 controller단에서 조회
     //지역과 지역이 아닌 해시태그 분리해서 조회
     //지역 해시태그 조회 is_city = true ,  isDelete = false
-    public List<HashtagResponseDTO> getHashtagsByIsCityTrue() {
-
-        return hashtagRepository.findByIsCityAndIsDelete(true, false)
-                .stream()
-                .map(Hashtag::toResponseDTO)
-                .collect(Collectors.toList());
-    }
+//    public List<HashtagResponseDTO> getHashtagsByIsCityTrue() {
+//
+//        return hashtagRepository.findByIsCityAndDelete(true, false);
+//    }
 
     //지역 아닌 해시태그 조회 is_city = false, isDelete = false
-    public List<HashtagResponseDTO> getHashtagsByIsCityFalse() {
-
-        return hashtagRepository.findByIsCityAndIsDelete(false, false)
-                .stream()
-                .map(Hashtag::toResponseDTO)
-                .collect(Collectors.toList());
-    }
+//    public List<HashtagResponseDTO> getHashtagsByIsCityFalse() {
+//
+//        return hashtagRepository.findByIsCityAndDelete(false, false);
+//    }
 
 
     //해시태그 수정
+    //QueryDSL X ,  JPA(더티체킹) 사용  -> DB에 존재하는 모든 값들이 채워지지 않았을 경우 Null 값으로 치환되지 않음
     @Transactional
     public HashtagResponseDTO updateHashtag(Long id, HashtagRequestDTO hashtagRequestDTO){
 
@@ -65,6 +61,7 @@ public class HashtagService {
     }
 
     //해시태그 삭제 ( isDelete = true 로 변경 )
+    @Transactional
     public void deleteHashtag(Long id){
 
         Hashtag hashtag = hashtagRepository.getById(id);
