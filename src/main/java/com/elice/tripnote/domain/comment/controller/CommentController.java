27de267dcad31471851896c86c521785e5035375
BugeonJo <bugeonjo91@gmail.com>
2024-05-27 15:50:21 +0900

    feat: 댓글 조회 controller 메서드 구현

--- src/main/java/com/elice/tripnote/domain/comment/controller/CommentController.java
+++ src/main/java/com/elice/tripnote/domain/comment/controller/CommentController.java
@@ -1,8 +1,61 @@
 package com.elice.tripnote.domain.comment.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.elice.tripnote.domain.comment.entity.CommentResponseDTO;
+import com.elice.tripnote.domain.comment.service.CommentService;
+import com.elice.tripnote.domain.post.exception.NoSuchPostException;
+import com.elice.tripnote.domain.post.exception.NoSuchUserException;
+import com.elice.tripnote.global.entity.ErrorResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
-public interface CommentController {
+@RequiredArgsConstructor
+public class CommentController implements SwaggerCommentController {
+
+    private final CommentService commentService;
+
+    @Override
+    @GetMapping("/api/user/comments")
+    public ResponseEntity<Page<CommentResponseDTO>> getCommentsByPostId(@RequestParam(name="postId") Long postId, @RequestParam(name="page", defaultValue = "1") int page, @RequestParam(name="size", defaultValue = "30") int size) {
+        return ResponseEntity.ok().body(commentService.getCommentsByPostId(postId, page, size));
+    }
+
+    @Override
+    @GetMapping("/api/admin/comments")
+    public ResponseEntity<Page<CommentResponseDTO>> getCommentsAll(@RequestParam(name="page", defaultValue = "1") int page, @RequestParam(name="size", defaultValue = "30") int size) {
+        return ResponseEntity.ok().body(commentService.getCommentsAll(page, size));
+    }
+
+    @Override
+    @GetMapping("/api/admin/comments/users/{memberId}")
+    public ResponseEntity<Page<CommentResponseDTO>> getCommentsByMemberId(@PathVariable(name="memberId") Long memberId, @RequestParam(name="page", defaultValue = "1") int page, @RequestParam(name="size", defaultValue = "30") int size) {
+        return ResponseEntity.ok().body(commentService.getCommentsByMemberId(memberId, page, size));
+    }
+
+
+    @ExceptionHandler(NoSuchPostException.class)
+    public ResponseEntity<ErrorResponse> handleNoSuchPostException(NoSuchPostException ex){
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .errorCode(404)
+                .message(ex.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(NoSuchUserException.class)
+    public ResponseEntity<ErrorResponse> handleNoSuchUserException(NoSuchUserException ex){
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .errorCode(404)
+                .message(ex.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
 }
--- src/main/java/com/elice/tripnote/domain/comment/controller/SwaggerCommentController.java
+++ src/main/java/com/elice/tripnote/domain/comment/controller/SwaggerCommentController.java
@@ -1,7 +1,69 @@
 package com.elice.tripnote.domain.comment.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.elice.tripnote.domain.comment.entity.CommentResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 
+@Tag(name = "Comment API", description = "댓글 API입니다.")
 public interface SwaggerCommentController {
+
+
+    @Operation(summary="게시글 댓글 조회 - 유저", description= "유저가 게시글에 해당하는 댓글을 조회할 때 사용하는 api입니다. 삭제되지 않은 댓글만 조회 가능합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "댓글 조회에 성공하였습니다.",  content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "댓글에 해당하는 게시판이 존재하지 않습니다.",  content = @Content(mediaType = "application/json"))
+    })
+    @Parameters(value = {
+            @Parameter(name="postId", description = "게시글 번호", example = "1123"),
+            @Parameter(name="page", description = "페이지 번호", example = "5"),
+            @Parameter(name="size", description = "페이지 크기", example = "30")
+    })
+
+    ResponseEntity<Page<CommentResponseDTO>> getCommentsByPostId(Long postId, int page, int size);
+
+
+    @Operation(summary="게시글 댓글 조회 - 관리자", description= "관리자가 모든 댓글을 조회할 때 사용하는 api입니다. 삭제된 댓글도 조회 가능합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "댓글 조회에 성공하였습니다.",  content = @Content(mediaType = "application/json"))
+    })
+    @Parameters(value = {
+            @Parameter(name="page", description = "페이지 번호", example = "5"),
+            @Parameter(name="size", description = "페이지 크기", example = "30")
+    })
+
+    ResponseEntity<Page<CommentResponseDTO>> getCommentsAll(int page, int size);
+
+
+    @Operation(summary="게시글 유저 댓글 조회 - 관리자", description= "관리자가 유저의 모든 댓글을 조회할 때 사용하는 api입니다. 삭제된 댓글도 조회 가능합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "댓글 조회에 성공하였습니다.",  content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "404", description = "해당하는 유저는 존재하지 않습니다.",  content = @Content(mediaType = "application/json"))
+    })
+    @Parameters(value = {
+            @Parameter(name="userId", description = "유저 번호", example = "1123"),
+            @Parameter(name="page", description = "페이지 번호", example = "5"),
+            @Parameter(name="size", description = "페이지 크기", example = "30")
+    })
+    ResponseEntity<Page<CommentResponseDTO>> getCommentsByMemberId(Long memberId, int page, int size);
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
