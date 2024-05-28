7a7ed153695d57a0aa7b09397cbc8ea6e13b9129
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2024-05-28 13:12:42 +0900

    remove: generated 삭제

--- src/main/java/com/elice/tripnote/domain/route/controller/RouteController.java
+++ src/main/java/com/elice/tripnote/domain/route/controller/RouteController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/routes")
+@RequestMapping("/api/member/routes")
 public class RouteController implements SwaggerRouteController{
     private final RouteService routeService;
 
