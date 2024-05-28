d3c993804aee6719169baf2947f1fdab0693a637
Kim Soo Hyun <spancer1@naver.com>
2024-05-28 22:49:16 +0900

    Implement Dummy Data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO spot (location, likes, image_url, region)
+VALUES
+("롯데월드", 10,"http://post.phinf.naver.net/MjAyMzAyMTNfMTU0/MDAxNjc2Mjc1MjU4NjQ0.wx9_WzFZPvjLf1zDlpU2aKyD1NivnDblqDhWca3FVtEg.34ndIbaEvAJHZ8hmX72_sTmgeJYNNGiIbrjejMNBkm0g.JPEG/I95fIsBVSHEbTxTbONflXSDVGcyQ.jpg", "서울"),
+("남산", 50,"http://post.phinf.naver.net/MjAyMzAyMTNfMTU0/MDAxNjc2Mjc1MjU4NjQ0.wx9_WzFZPvjLf1zDlpU2aKyD1NivnDblqDhWca3FVtEg.34ndIbaEvAJHZ8hmX72_sTmgeJYNNGiIbrjejMNBkm0g.JPEG/I95fIsBVSHEbTxTbONflXSDVGcyQ.jpg", "서울"),
+("경복궁", 20,"http://post.phinf.naver.net/MjAyMzAyMTNfMTU0/MDAxNjc2Mjc1MjU4NjQ0.wx9_WzFZPvjLf1zDlpU2aKyD1NivnDblqDhWca3FVtEg.34ndIbaEvAJHZ8hmX72_sTmgeJYNNGiIbrjejMNBkm0g.JPEG/I95fIsBVSHEbTxTbONflXSDVGcyQ.jpg", "서울"),
+("63빌딩", 30,"http://post.phinf.naver.net/MjAyMzAyMTNfMTU0/MDAxNjc2Mjc1MjU4NjQ0.wx9_WzFZPvjLf1zDlpU2aKyD1NivnDblqDhWca3FVtEg.34ndIbaEvAJHZ8hmX72_sTmgeJYNNGiIbrjejMNBkm0g.JPEG/I95fIsBVSHEbTxTbONflXSDVGcyQ.jpg", "서울"),
+("롯데타워", 5,"http://post.phinf.naver.net/MjAyMzAyMTNfMTU0/MDAxNjc2Mjc1MjU4NjQ0.wx9_WzFZPvjLf1zDlpU2aKyD1NivnDblqDhWca3FVtEg.34ndIbaEvAJHZ8hmX72_sTmgeJYNNGiIbrjejMNBkm0g.JPEG/I95fIsBVSHEbTxTbONflXSDVGcyQ.jpg", "서울"),
+("노티드 도넛", 12,"http://post.phinf.naver.net/MjAyMzAyMTNfMTU0/MDAxNjc2Mjc1MjU4NjQ0.wx9_WzFZPvjLf1zDlpU2aKyD1NivnDblqDhWca3FVtEg.34ndIbaEvAJHZ8hmX72_sTmgeJYNNGiIbrjejMNBkm0g.JPEG/I95fIsBVSHEbTxTbONflXSDVGcyQ.jpg", "서울"),
+("랜디스 도넛", 78,"http://post.phinf.naver.net/MjAyMzAyMTNfMTU0/MDAxNjc2Mjc1MjU4NjQ0.wx9_WzFZPvjLf1zDlpU2aKyD1NivnDblqDhWca3FVtEg.34ndIbaEvAJHZ8hmX72_sTmgeJYNNGiIbrjejMNBkm0g.JPEG/I95fIsBVSHEbTxTbONflXSDVGcyQ.jpg", "서울"),
+("길동시장", 23,"http://post.phinf.naver.net/MjAyMzAyMTNfMTU0/MDAxNjc2Mjc1MjU4NjQ0.wx9_WzFZPvjLf1zDlpU2aKyD1NivnDblqDhWca3FVtEg.34ndIbaEvAJHZ8hmX72_sTmgeJYNNGiIbrjejMNBkm0g.JPEG/I95fIsBVSHEbTxTbONflXSDVGcyQ.jpg", "서울"),
+("뚝섬한강공원", 65,"http://post.phinf.naver.net/MjAyMzAyMTNfMTU0/MDAxNjc2Mjc1MjU4NjQ0.wx9_WzFZPvjLf1zDlpU2aKyD1NivnDblqDhWca3FVtEg.34ndIbaEvAJHZ8hmX72_sTmgeJYNNGiIbrjejMNBkm0g.JPEG/I95fIsBVSHEbTxTbONflXSDVGcyQ.jpg", "서울"),
+("망원한강공원", 101,"http://post.phinf.naver.net/MjAyMzAyMTNfMTU0/MDAxNjc2Mjc1MjU4NjQ0.wx9_WzFZPvjLf1zDlpU2aKyD1NivnDblqDhWca3FVtEg.34ndIbaEvAJHZ8hmX72_sTmgeJYNNGiIbrjejMNBkm0g.JPEG/I95fIsBVSHEbTxTbONflXSDVGcyQ.jpg", "서울");
+
+INSERT INTO spot (location, likes, image_url, region)
+VALUES
+("에버랜드", 135,"http://post.phinf.naver.net/MjAyMzAyMTNfMTU0/MDAxNjc2Mjc1MjU4NjQ0.wx9_WzFZPvjLf1zDlpU2aKyD1NivnDblqDhWca3FVtEg.34ndIbaEvAJHZ8hmX72_sTmgeJYNNGiIbrjejMNBkm0g.JPEG/I95fIsBVSHEbTxTbONflXSDVGcyQ.jpg", "경기"),
+("캐리비안베이", 50,"http://post.phinf.naver.net/MjAyMzAyMTNfMTU0/MDAxNjc2Mjc1MjU4NjQ0.wx9_WzFZPvjLf1zDlpU2aKyD1NivnDblqDhWca3FVtEg.34ndIbaEvAJHZ8hmX72_sTmgeJYNNGiIbrjejMNBkm0g.JPEG/I95fIsBVSHEbTxTbONflXSDVGcyQ.jpg", "경기도"),
+("서울랜드", 20,"http://post.phinf.naver.net/MjAyMzAyMTNfMTU0/MDAxNjc2Mjc1MjU4NjQ0.wx9_WzFZPvjLf1zDlpU2aKyD1NivnDblqDhWca3FVtEg.34ndIbaEvAJHZ8hmX72_sTmgeJYNNGiIbrjejMNBkm0g.JPEG/I95fIsBVSHEbTxTbONflXSDVGcyQ.jpg", "경기도");
