c3a0d8fedf7fbaacd5f126c3b04f9a53b588e1dc
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2024-05-29 14:37:33 +0900

    feat, remove: exception 설정

--- src/main/java/com/elice/tripnote/global/exception/ErrorCode.java
+++ src/main/java/com/elice/tripnote/global/exception/ErrorCode.java
@@ -9,7 +9,14 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    // 404
     NO_POST(HttpStatus.NOT_FOUND, "해당하는 게시글은 존재하지 않습니다."),
+    NO_MEMBER(HttpStatus.NOT_FOUND, "해당하는 유저는 존재하지 않습니다."),
+    NO_SPOT(HttpStatus.NOT_FOUND, "해당하는 여행지가 존재하지 않습니다."),
+    NO_ROUTE(HttpStatus.NOT_FOUND, "해당하는 경로가 존재하지 않습니다."),
+    NOT_FOUND_ALGORITHM(HttpStatus.NOT_FOUND, "SHA-1 알고리즘을 찾을 수 없습니다."),
+
+    // 409
     DUPLICATE_NAME(HttpStatus.CONFLICT, "중복되는 해시태그명입니다.");
 
     private final HttpStatus httpStatus;
