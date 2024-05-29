438d5e2aafdb04e4cc486d4b60903fed7dd22484
Kim Soo Hyun <spancer1@naver.com>
2024-05-29 16:39:40 +0900

    여행지 조회 불가 예외 구현

--- src/main/java/com/elice/tripnote/domain/spot/exception/LandmarkNotFoundException.java
+++ src/main/java/com/elice/tripnote/domain/spot/exception/LandmarkNotFoundException.java
@@ -1,2 +1,11 @@
-package com.elice.tripnote.domain.spot.exception;public class LandmarkNotFoundException {
+package com.elice.tripnote.domain.spot.exception;
+
+import com.elice.tripnote.global.exception.NotFoundException;
+
+public class LandmarkNotFoundException extends NotFoundException {
+    private final static String message = "존재하지 않는 랜드마크입니다.";
+
+    public LandmarkNotFoundException(){
+        super(message);
+    }
 }
