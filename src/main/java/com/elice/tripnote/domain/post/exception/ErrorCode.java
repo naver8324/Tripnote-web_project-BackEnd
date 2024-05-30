9be6a36d7b19fbb8afd68be6b3d9a4407b3f3138
BugeonJo <bugeonjo91@gmail.com>
2024-05-30 16:54:45 +0900

    feat: 경로 적절성에 대한 예외 추가

--- src/main/java/com/elice/tripnote/domain/post/exception/ErrorCode.java
+++ src/main/java/com/elice/tripnote/domain/post/exception/ErrorCode.java
@@ -16,6 +16,7 @@ public enum ErrorCode {
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "해당 실행을 수행할 권한이 없습니다."),
     EXCEED_SIZE_LIMIT(HttpStatus.BAD_REQUEST, "파일 크기가 범위를 넘었습니다."),
     NOT_MATCHED_TYPE(HttpStatus.BAD_REQUEST, "이미지가 아닌 파일입니다."),
+    NOT_VALID_ROUTE(HttpStatus.BAD_REQUEST, "이 경로는 비공개, 삭제되었거나 유저의 경로가 아닙니다."),
     NO_FILE(HttpStatus.NOT_FOUND, "해당 이미지가 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
