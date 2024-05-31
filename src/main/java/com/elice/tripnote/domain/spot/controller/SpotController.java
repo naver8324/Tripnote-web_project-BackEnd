7a22332bf6b0909ba62ca34180e7953ec8faa281
Kim Soo Hyun <spancer1@naver.com>
2024-05-31 15:36:11 +0900

    특정 여행지 좋아요 증가, 감소 API 구현

--- src/main/java/com/elice/tripnote/domain/spot/controller/SpotController.java
+++ src/main/java/com/elice/tripnote/domain/spot/controller/SpotController.java
@@ -5,6 +5,7 @@ import com.elice.tripnote.domain.spot.dto.SpotDTO;
 import com.elice.tripnote.domain.spot.dto.SpotResponseDTO;
 import com.elice.tripnote.domain.spot.entity.Spot;
 import com.elice.tripnote.domain.spot.exception.LandmarkNotFoundException;
+import com.elice.tripnote.domain.spot.exception.RegionNotFoundException;
 import com.elice.tripnote.domain.spot.service.SpotService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -25,36 +27,33 @@ public class SpotController implements SwaggerSpotController{
     @Override
     @GetMapping
     public ResponseEntity<?> getSpots(@RequestParam(required = false, name="region") String region,
-                                      @RequestParam(required = false, name = "landmark") String landmark) {
-        if (region != null && landmark != null) {
-            return new ResponseEntity<>("Both region and landmark cannot be specified", HttpStatus.BAD_REQUEST);
-        }
-        if (region != null) {
-            return getSpotsByRegion(region);
-        } else if (landmark != null) {
-            return getSpotsByLandmark(landmark);
-        } else {
-            return new ResponseEntity<>("Either region or landmark must be specified", HttpStatus.BAD_REQUEST);
-        }
-    }
-    private ResponseEntity<List<Spot>> getSpotsByRegion(String region) {
-        try {
-            Region validRegion = Region.fromString(region);
-            List<Spot> list = spotService.getSpotsByRegion(validRegion.getName(), 0, 5);
-            if (!list.isEmpty()) {
-                return new ResponseEntity<>(list, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                                      @RequestParam(required = false, name = "location") String location) {
+        try{
+            if (region != null && location != null) {
+                return new ResponseEntity<>("Both region and landmark cannot be specified", HttpStatus.BAD_REQUEST);
             }
-        } catch (IllegalArgumentException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            if(region !=null)
+                return getSpotsByRegion(region);
+            else
+                return getSpotsByLocation(location);
+        }catch(LandmarkNotFoundException | RegionNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
-    private ResponseEntity<Spot> getSpotsByLandmark(String landmark) {
-        if(spotService.searchByLocation(landmark) !=null){
-            return ResponseEntity.ok().body(spotService.searchByLocation(landmark));
+
+    private ResponseEntity<List<Spot>> getSpotsByRegion(String region) {
+        Region validRegion = Region.fromString(region);
+        List<Spot> list = spotService.getSpotsByRegion(validRegion.getName(),0,5);
+        if(list.isEmpty())
+            throw new RegionNotFoundException();
+        return new ResponseEntity<>(list,HttpStatus.OK);
+    }
+
+    private ResponseEntity<Spot> getSpotsByLocation(String location) {
+        if (spotService.searchByLocation(location) != null) {
+            return ResponseEntity.ok().body(spotService.searchByLocation(location));
         }
-        SpotDTO result = spotService.search(landmark);
+        SpotDTO result = spotService.search(location);
         if (result == null) {
             throw new LandmarkNotFoundException();
         }
@@ -65,40 +64,42 @@ public class SpotController implements SwaggerSpotController{
     @Override
     @GetMapping("/{id}")
     public ResponseEntity<Spot> getSpotById(@PathVariable("id") Long id){
-        Spot spot = spotService.searchById(id);
-        return ResponseEntity.ok().body(spot);
-    }
-    @Override
-    @PostMapping
-    public ResponseEntity<Spot> add(@RequestBody SpotDTO spotDTO) {
-        log.info("{}", spotDTO);
-        Spot createdSpot = spotService.add(spotService.dtoToEntity(spotDTO));
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdSpot);
-    }
-
-    @Override
-    @PatchMapping("/increaseLikes")
-    public ResponseEntity<Spot> increaseLike(@RequestParam(name = "landmark") String landmark) {
-        try {
-            Spot updatedSpot = spotService.increaseLike(landmark);
-            return ResponseEntity.ok().body(updatedSpot);
-        } catch (LandmarkNotFoundException e) {
+        try{
+            Spot spot = spotService.searchById(id);
+            return ResponseEntity.ok().body(spot);
+        }catch(LandmarkNotFoundException e){
+            log.error("에러 발생: {}", e.getMessage(), e);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
     @Override
-    @PatchMapping("/decreaseLikes")
-    public ResponseEntity<Spot> decreaseLike(@RequestParam(name = "landmark") String landmark) {
+    @PostMapping
+    public ResponseEntity<Spot> add(@RequestBody SpotDTO spotDTO) {
+        log.info("{}", spotDTO);
+        Spot createdSpot = spotService.add(spotDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdSpot);
+    }
+
+    @Override
+    @PatchMapping("/increaseLikes")
+    public ResponseEntity<Void> increaseLike(@RequestParam(name = "location") String location) {
         try {
-            Spot updatedSpot = spotService.decreaseLike(landmark);
-            if (updatedSpot == null) {
-                throw new LandmarkNotFoundException();
-            } else {
-                return ResponseEntity.ok().body(updatedSpot);
-            }
+            spotService.increaseLike(location);
+            return ResponseEntity.ok().build();
         } catch (LandmarkNotFoundException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @Override
+    @PatchMapping("/decreaseLikes")
+    public ResponseEntity<Void> decreaseLike(@RequestParam(name = "location") String location) {
+        try {
+            spotService.decreaseLike(location);
+            return ResponseEntity.ok().build();
+        } catch (LandmarkNotFoundException e) {
+            return ResponseEntity.badRequest().build();
         }
     }
 }
