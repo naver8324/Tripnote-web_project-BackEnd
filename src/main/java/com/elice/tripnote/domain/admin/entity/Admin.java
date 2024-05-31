c9a921b6684fbe218b12c37a7f6cc818d4d4a182
[Cloud2] 정주용 <dusk3236@gmail.com>
2024-05-31 09:19:42 +0900

    feat: 관리자 로그인 구현, admin Entity 필드명 수정 #29

--- src/main/java/com/elice/tripnote/domain/admin/entity/Admin.java
+++ src/main/java/com/elice/tripnote/domain/admin/entity/Admin.java
@@ -2,21 +2,21 @@ package com.elice.tripnote.domain.admin.entity;
 
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Table(name = "admin")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
+@Builder
 public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
-    private String login_id;
+    @Column(name = "login_id", nullable = false, unique = true)
+    private String loginId;
 
     @Column(nullable = false)
     private String password;
