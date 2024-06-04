35d25abc450854d862a9b4bcbd5b522cf336f8d4
Kim Soo Hyun <spancer1@naver.com>
2024-06-05 00:01:47 +0900

    Refactoring Dummy Data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,40 +1,13 @@
-INSERT INTO spot (location, likes, image_url, region)
-VALUES
-("롯데월드", 10,"https://adventure.lotteworld.com/common/images/icon/introCon_img0.jpg", "서울특별시"),
-("남산", 50,"https://image6.yanolja.com/leisure/dC0u98BeFbTx9VaY/w800", "서울특별시"),
-("경복궁", 20,"https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/323M/image/Ldsfv84iiqXP4PIAph8UVgDYBrs.jpeg", "서울특별시"),
-("63빌딩", 30,"https://i.pinimg.com/736x/c9/9e/c2/c99ec2f4c944fb7622a0b800159b392b.jpg", "서울특별시"),
-("롯데타워", 5,"http://imgnews.naver.net/image/5818/2020/07/29/0000015634_004_20200729192111768.jpg", "서울특별시"),
-("코엑스", 12,"http://post.phinf.naver.net/MjAyMzExMTNfMTMx/MDAxNjk5ODQ2MzgzNTQ1.rfkbtyUGOk9IBtDgkz4QRbyVkpsrluJtLbR95gPmLE0g.xtAzYHc-QqlZ6sk1sEXepChB65beN9ed8UWZkmTSt_Ug.JPEG/I-47MJxGYK7qZbFnP5BoKCazre3k.jpg", "서울특별시"),
-("랜디스도넛 안국점", 78,"https://ldb-phinf.pstatic.net/20220118_121/1642484145557s9lhY_JPEG/KakaoTalk_20220118_142759493_03.jpg", "서울특별시"),
-("청계천", 23,"https://mediahub.seoul.go.kr/uploads/mediahub/2022/08/YWvHnsIbRmJoxcaXQwrvVYfAfqPQEUxZ.png?ver=1.0", "서울특별시"),
-("광화문", 65,"https://d2u3dcdbebyaiu.cloudfront.net/uploads/atch_img/700/14d64ff6b39cfb0477f381dcac36524f_res.jpeg", "서울특별시"),
-("망원 한강공원", 101,"http://post.phinf.naver.net/MjAyMDAzMTdfMTU5/MDAxNTg0NDM3Njc3MjY0.E8t8v5DV5BDTLyjsWmm1bFI1Gtiy9sCLy39qA4ZAqScg.917tErxw_h20edl4r2yU7odWq8oY7tmg90MMU6XTQk8g.JPEG/IQSJ3FR8cnfSx-3Br87kYbvHF1Z4.jpg", "서울특별시");
+INSERT INTO spot (location, image_url, region, address, lat, lng) VALUES
+("남산", "https://image6.yanolja.com/leisure/dC0u98BeFbTx9VaY/w800", "SEOUL", "서울특별시 용산구 용산동2가 산1-3", 1269882475.0, 375512168.0),
+("경복궁", "https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/323M/image/Ldsfv84iiqXP4PIAph8UVgDYBrs.jpeg", "SEOUL", "서울특별시 종로구 세종로 1-91 경복궁", 1269770162.0, 375788407.0),
+("청계천", "https://mediahub.seoul.go.kr/uploads/mediahub/2022/08/YWvHnsIbRmJoxcaXQwrvVYfAfqPQEUxZ.png?ver=1.0", "SEOUL", "서울특별시 종로구 창신동", 1270056375.0, 375696512.0),
+("광화문", "https://d2u3dcdbebyaiu.cloudfront.net/uploads/atch_img/700/14d64ff6b39cfb0477f381dcac36524f_res.jpeg", "SEOUL", "서울특별시 종로구 세종로 1-57", 1269768428.0, 375760259.0),
+("숭례문", "https://cdn.crowdpic.net/detail-thumb/thumb_d_2112CAEB5B47138C30CA5C14E4123BE2.jpg", "SEOUL", "서울특별시 중구 남대문로4가 29", 1269753562.0, 375599757.0),
+("망원 한강공원", "http://post.phinf.naver.net/MjAyMDAzMTdfMTU5/MDAxNTg0NDM3Njc3MjY0.E8t8v5DV5BDTLyjsWmm1bFI1Gtiy9sCLy39qA4ZAqScg.917tErxw_h20edl4r2yU7odWq8oY7tmg90MMU6XTQk8g.JPEG/IQSJ3FR8cnfSx-3Br87kYbvHF1Z4.jpg", "SEOUL", "서울특별시 마포구 망원동 205-4", 1268952798.0, 375553018.0),
+("북촌 한옥마을", "http://post.phinf.naver.net/MjAyNDAzMjdfNjEg/MDAxNzExNTE4MzkxMzQw.OiqJYtWrdhrGK2fcE8jjcNTSA5fCDzblQMYZ4JXtDhYg.GkXnGorH1Txvr3EaTV1u7oUU8ATphEMe6kuvhStKwnUg.JPEG/IFFQ-cg2VOU3a7C4wegKK_c2XMWs.jpg", "SEOUL", "서울특별시 종로구 계동 105", 1269850839.0, 375813540.0),
+("여의도 한강공원", "http://post.phinf.naver.net/MjAxNzA5MDhfMjgw/MDAxNTA0ODMxODI4OTYw.CXBA0oXZhXklXJ3rB0FiOpnSLLZmT5v8-E510NVRb-4g.u3QsnxhZItByouOHNNJYQhnJ14nWjcYYdhF6hYgmojAg.JPEG/IrovESbYNemfm_Cw92LF8yh5hd5M.jpg", "SEOUL", "서울특별시 영등포구 여의도동 8", 1269348268.0, 375263232.0),
+("덕수궁", "http://dh.aks.ac.kr/Encyves/wiki/images/0/01/%ED%95%9C%EA%B8%80%ED%8C%80_%EB%8D%95%EC%88%98%EA%B6%81_%EC%A4%91%ED%99%94%EC%A0%84.jpg", "SEOUL", "서울특별시 중구 정동 5-1 덕수궁", 1269749148.0, 375660945.0),
+("올림픽공원", "https://mediahub.seoul.go.kr/uploads/mediahub/2023/10/SnLPydvyqzdpFaNmlxCIhJPCqKqvGMcE.jpg", "SEOUL", "서울특별시 송파구 방이동 88-3", 1271229590.0, 375209339.0),
+("서울숲", "https://mediahub.seoul.go.kr/uploads/mediahub/2024/04/jVtcxjLLRnVssFSCtBPRRvJjoBMwwzIm.jpg", "SEOUL", "서울특별시 성동구 성수동1가 685-20", 1270385663.0, 375419644.0);
 
-INSERT INTO spot (location, likes, image_url, region)
-VALUES
-("에버랜드", 135,"http://post.phinf.naver.net/MjAyMzAyMTNfMTU0/MDAxNjc2Mjc1MjU4NjQ0.wx9_WzFZPvjLf1zDlpU2aKyD1NivnDblqDhWca3FVtEg.34ndIbaEvAJHZ8hmX72_sTmgeJYNNGiIbrjejMNBkm0g.JPEG/I95fIsBVSHEbTxTbONflXSDVGcyQ.jpg", "경기도"),
-("캐리비안베이", 50,"https://dnvefa72aowie.cloudfront.net/origin/article/202208/2104f58098b19c57e038f124ad965fef404c3d57436015a6671fab6e466fb1f4.webp?q=95&s=1440x1440&t=inside", "경기도"),
-("수원화성", 20,"https://dbscthumb-phinf.pstatic.net/5701_000_47/20240312190558486_ACR6HRW4Q.jpg/a29zYjEzfDM0NzI0.jpg?type=m936", "경기도"),
-("한국민속촌", 15,"https://ldb-phinf.pstatic.net/20240102_42/1704184142264XSIgd_PNG/KakaoTalk_20240102_172035962_01.png", "경기도"),
-("서울대공원", 74,"http://imgnews.naver.net/image/003/2023/04/04/NISI20230404_0019844531_web_20230404153855_20230404154030233.jpg", "경기도");
-
-INSERT INTO spot (location, likes, image_url, region)
-VALUES
-("해운대", 135,"https://www.busan.go.kr/comm/getFile?srvcId=BBSTY3&upperNo=1601007&fileNo=1", "부산광역시"),
-("광안리해수욕장", 50,"https://r1.community.samsung.com/t5/image/serverpage/image-id/1376436i916AAF580A6FED84/image-size/large?v=v2&px=999", "부산광역시"),
-("감천문화마을", 20,"http://post.phinf.naver.net/MjAyMjExMTBfMjUw/MDAxNjY4MDY3MDU0NjEz.599-F6ySZrjNyHPuSR4P0ejrhHK-Qrgh25_Nr-PaRbIg.NlaGzaJ4xfBkGJYcU_WHJvX9LOA-1kdyjiVwizNFJNIg.JPEG/IK9rUdP8q-FyC_36j8Bbu-7E2epM.jpg", "부산광역시"),
-("롯데월드 어드벤처 부산", 15,"http://imgnews.naver.net/image/018/2022/03/31/0005179446_001_20220331205101083.jpg", "부산광역시");
-
-INSERT INTO spot (location, likes, image_url, region)
-VALUES
-("한라산", 256,"http://imgnews.naver.net/image/5522/2024/01/03/0000042338_001_20240103085002381.png", "제주특별자치도"),
-("섭지코지", 187,"http://post.phinf.naver.net/MjAxODA0MTBfMjgy/MDAxNTIzMjg3NDkxNDM2.jX7NAJ7NETrimnFt0OlHu-isqXxbycSj4jZ0HsK73tIg.pi2Qjg2zfHgUmHau3UdLnuml6gvxoqxjli-8Fn9OzsAg.JPEG/Is1KeponIhbZLVZpLynqFLgXSVoA.jpg", "제주특별자치도"),
-("성산일출봉", 398,"https://i.pinimg.com/originals/00/d3/e4/00d3e4bce96621a254d3fcc559531845.jpg", "제주특별자치도"),
-("산방산", 122,"http://post.phinf.naver.net/MjAxODAyMjdfMjg5/MDAxNTE5NzIxMTQ2NTAw.BKQyoUaWgCmTGLYkEPathzBk9eZCQfp35FMHYzUjKmEg.7uH-9J8GKfKKvc0qbRNIQ8nMGobpkQKMBcAdTQ3IMBkg.JPEG/IrDVxSZkfQtxcvjVb-udJNQpGvvM.jpg", "제주특별자치도"),
-("용머리해안", 165,"https://pup-review-phinf.pstatic.net/MjAyMzA0MTFfMjA2/MDAxNjgxMTk2NDQ5MTg0.TP6gxXqBVH5w6GPUeMvoadY2f9UtkvdYtetd6Tk9yVMg.wCY07Mc9unM7UbuSmZQ0OfOzeEKLBzHSoMng50ewOdcg.JPEG/20230408_152550.jpg?type=w828_60_sharpen", "제주특별자치도"),
-("스누피가든", 87,"http://post.phinf.naver.net/MjAyMjA2MDNfMjk4/MDAxNjU0MjUzODk2MDQ2.8YY7XCcUxL1CcMrZVvvqaXSY77rTz9veD0qrYvMh7HUg.U4eknWw-Eidfpiz94eOWbXntNbwtGeTRC0llYB4nP2gg.PNG/IFoUB6HzxGm42jKUhzgCto7KpH0k.jpg", "제주특별자치도"),
-("해일리 카페", 348,"https://ldb-phinf.pstatic.net/20230914_118/1694666985904Ho5cP_JPEG/TalkMedia_i_ec9e9331aa59.jpeg.jpeg", "제주특별자치도"),
-("함덕해수욕장", 689,"https://modo-phinf.pstatic.net/20220723_250/1658567763580B0lMD_JPEG/mosankULuy.jpeg", "제주특별자치도"),
-("협재해수욕장", 512,"https://thumb2.gettyimageskorea.com/image_preview/700/201908/MBRF/MBRF19018137.jpg", "제주특별자치도"),
-("천지연폭포", 328,"https://dbscthumb-phinf.pstatic.net/4751_000_6/20170126231255178_2992CV3T9.jpg/A-H-E1S-D0-0201-.jpg?type=m4500_4500_fst", "제주특별자치도");
