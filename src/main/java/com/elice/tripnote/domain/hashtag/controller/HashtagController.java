881b1c4563827a28d81b1f155c48503df3754f5c
yukisaha <0412judy@gmail.com>
2024-06-05 16:35:14 +0900

    feat: @AdminRole @MemberRole 추가

--- src/main/java/com/elice/tripnote/domain/hashtag/controller/HashtagController.java
+++ src/main/java/com/elice/tripnote/domain/hashtag/controller/HashtagController.java
@@ -5,6 +5,7 @@ import com.elice.tripnote.domain.hashtag.entity.HashtagRequestDTO;
 import com.elice.tripnote.domain.hashtag.entity.HashtagResponseDTO;
 import com.elice.tripnote.domain.hashtag.repository.HashtagRepository;
 import com.elice.tripnote.domain.hashtag.service.HashtagService;
+import com.elice.tripnote.global.annotation.AdminRole;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/admin/hashtags")
+@RequestMapping("/api")
 public class HashtagController implements SwaggerHashtagController{
 
     private final HashtagService hashtagService;
@@ -22,7 +23,7 @@ public class HashtagController implements SwaggerHashtagController{
 
     //전체 해시태그 조회
     @Override
-    @GetMapping
+    @GetMapping("/hashtags")
     public ResponseEntity<List<HashtagDTO>> getHashtags(){
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -34,7 +35,7 @@ public class HashtagController implements SwaggerHashtagController{
     //지역 아닌 해시태그 조회 isCity의 = false, isDelete = false
     //프론트에서 isCity의 값을 받아온다
     @Override
-    @GetMapping("/isCity")
+    @GetMapping("/hashtags/isCity")
     public ResponseEntity<List<HashtagResponseDTO>> getHashtagsByIsCityTrue(@RequestParam(name = "isCity") boolean isCity,
                                                                             @RequestParam(name = "isDelete") boolean isDelete){
 
@@ -45,7 +46,8 @@ public class HashtagController implements SwaggerHashtagController{
 
     //해시태그 생성
     @Override
-    @PostMapping("/create")
+    @AdminRole
+    @PostMapping("/admin/hashtags/create")
     public ResponseEntity<HashtagResponseDTO> createHashtag(@RequestBody HashtagRequestDTO hashtagRequestDTO){
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -54,7 +56,8 @@ public class HashtagController implements SwaggerHashtagController{
 
     //해시태그 수정
     @Override
-    @PatchMapping("/update/{id}")
+    @AdminRole
+    @PatchMapping("/admin/hashtags/update/{id}")
     public ResponseEntity<HashtagResponseDTO> updateHashtag(@PathVariable Long id, @RequestBody HashtagRequestDTO hashtagRequestDTO){
         return ResponseEntity
                 .status(HttpStatus.OK)
@@ -62,7 +65,8 @@ public class HashtagController implements SwaggerHashtagController{
     }
 
     //해시태그 삭제
-    @DeleteMapping("delete/{id}")
+    @AdminRole
+    @DeleteMapping("/admin/hashtags/delete/{id}")
     public ResponseEntity<Void> deleteHashtag(@PathVariable Long id){
 
         hashtagService.deleteHashtag(id);
