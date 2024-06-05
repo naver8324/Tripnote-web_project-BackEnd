993a8059b79c31d40cb3dc92e8ab44e8d9969bfc
yukisaha <0412judy@gmail.com>
2024-06-05 17:45:15 +0900

    remove: swagger에서 불필요한 파라미터 제거

--- src/main/java/com/elice/tripnote/domain/hashtag/controller/SwaggerHashtagController.java
+++ src/main/java/com/elice/tripnote/domain/hashtag/controller/SwaggerHashtagController.java
@@ -23,8 +23,7 @@ public interface SwaggerHashtagController {
     @Operation(summary = "해시태그 조회", description = "도시 또는 도시가 아닌 해시태그를 조회합니다.")
     @ApiResponse(responseCode = "200", description = "성공")
     @Parameters(value = {
-            @Parameter(name="isCity", description = "지역여부", example = "true, false"),
-            @Parameter(name="isDelete", description = "삭제여부", example = "true, false")
+            @Parameter(name="isCity", description = "지역여부", example = "true, false")
     })
     ResponseEntity<List<HashtagResponseDTO>> getHashtagsByIsCityTrue(boolean isCity);
 
