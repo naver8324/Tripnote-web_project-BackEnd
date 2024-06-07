8e7c2e4bd8bb1f7f73fd93d7c75a0a944ab36c7f
BugeonJo <bugeonjo91@gmail.com>
2024-06-07 11:37:22 +0900

    fix: 댓글 생성 컨트롤러 수정

--- src/main/java/com/elice/tripnote/domain/comment/controller/CommentController.java
+++ src/main/java/com/elice/tripnote/domain/comment/controller/CommentController.java
@@ -46,7 +46,6 @@ public class CommentController implements SwaggerCommentController {
     @MemberRole
     @PostMapping("/member/comments")
     public ResponseEntity<CommentResponseDTO> saveComment(@Valid @RequestBody CommentRequestDTO commentDTO, @RequestParam(name="postId") Long postId) {
-        commentService.saveComment(commentDTO, postId);
         return ResponseEntity.status(HttpStatus.CREATED).body(commentService.saveComment(commentDTO, postId));
     }
 
