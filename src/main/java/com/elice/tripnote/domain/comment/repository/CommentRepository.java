4fd7ec9977033fa704bbba32db95537e5c8c7125
yukisaha <0412judy@gmail.com>
2024-06-11 02:38:27 +0900

    refactor: comment pageRequestDTO 적용

--- src/main/java/com/elice/tripnote/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/elice/tripnote/domain/comment/repository/CommentRepository.java
@@ -2,11 +2,7 @@ package com.elice.tripnote.domain.comment.repository;
 
 
 import com.elice.tripnote.domain.comment.entity.Comment;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, CustomCommentRepository{
 
