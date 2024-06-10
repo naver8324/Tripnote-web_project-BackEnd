f17b734dace29785bcc0f0aca04319c567b988f0
yukisaha <0412judy@gmail.com>
2024-06-10 18:32:57 +0900

    fix: swagger 수정

--- src/main/java/com/elice/tripnote/domain/admin/controller/SwaggerAdminController.java
+++ src/main/java/com/elice/tripnote/domain/admin/controller/SwaggerAdminController.java
@@ -10,8 +10,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,19 +19,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface SwaggerAdminController {
 
 
-    //@Operation(summary = "회원 목록 조회", description = "회원 목록을 페이지네이션하여 조회합니다.",
-    //        parameters = {
-    //                @Parameter(name = "page", description = "페이지 번호 (0부터 시작)", schema = @Schema(type = "integer", defaultValue = "0")),
-    //                @Parameter(name = "size", description = "페이지 크기", schema = @Schema(type = "integer", defaultValue = "10")),
-    //                @Parameter(name = "sort", description = "정렬 기준. 예: id", schema = @Schema(type = "string", defaultValue = "id"))
-    //        })
-    //@ApiResponses(value = {
-    //        @ApiResponse(responseCode = "200", description = "회원 목록 조회에 성공하였습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Page.class))),
-    //        @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자입니다. (토큰 값이 제대로 전달되었는지 확인이 필요합니다.)", content = @Content(mediaType = "application/json")),
-    //        @ApiResponse(responseCode = "403", description = "관리자 권한이 없습니다.", content = @Content(mediaType = "application/json"))
-    //})
-    //@GetMapping("/admin/members")
-    //ResponseEntity<Page<MemberResponseDTO>> getMembers(PageRequestDTO pageRequestDTO);
+    @Operation(summary = "회원 목록 조회", description = "회원 목록을 페이지네이션하여 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원 목록 조회에 성공하였습니다.", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Page.class))),
+            @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자입니다. (토큰 값이 제대로 전달되었는지 확인이 필요합니다.)", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "403", description = "관리자 권한이 없습니다.", content = @Content(mediaType = "application/json"))
+    })
+    @GetMapping("/admin/members")
+    ResponseEntity<Page<MemberResponseDTO>> getMembers(PageRequestDTO pageRequestDTO);
 
     @Operation(summary = "회원 삭제", description = "이메일을 통해 회원을 삭제합니다.",
             parameters = @Parameter(name = "email", description = "삭제할 회원의 이메일", required = true, schema = @Schema(type = "string")))
