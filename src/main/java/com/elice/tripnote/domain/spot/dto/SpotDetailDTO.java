6368105e6d50be5e17068ebf1722d66f977dfded
yukisaha <0412judy@gmail.com>
2024-06-13 15:22:48 +0900

    feat: 반환값이 json형식으로 나오지 않아 기존 Spot 엔티티를 반환하던 spotDetaildTO를 spotDTO를 반환하도록 변경

--- src/main/java/com/elice/tripnote/domain/spot/dto/SpotDetailDTO.java
+++ src/main/java/com/elice/tripnote/domain/spot/dto/SpotDetailDTO.java
@@ -1,10 +1,12 @@
 package com.elice.tripnote.domain.spot.dto;
 
 import com.elice.tripnote.domain.spot.entity.Spot;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Data
@@ -12,5 +14,5 @@ import java.util.Map;
 @AllArgsConstructor
 public class SpotDetailDTO {
     private Spot spot;
-    private Map<Spot, Double> nextSpots;
+    private Map<SpotDTO, Double> nextSpots;
 }
